4ca8d6972f2725247c1f3cbcbdcd1d432de46ba4
sadrazkh <sadra.z.kh@gmail.com>
2024-03-15 17:41:11 +0330

    Complited

--- ReadHierarchicalExcelFile/Program.cs
+++ ReadHierarchicalExcelFile/Program.cs
@@ -2,22 +2,17 @@
 using OfficeOpenXml;
 
 
-// Path to your Excel file
 string filePath = @"C:\Users\sadra\source\repos\ReadHierarchicalExcelFile\ReadHierarchicalExcelFile\test.xlsx";
 
-// Load the Excel file
 using (ExcelPackage package = new ExcelPackage(new System.IO.FileInfo(filePath)))
 {
-    // Assuming the data is in the first worksheet
     ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
 
-    // Get the number of rows in the worksheet
     int rowCount = worksheet.Dimension.Rows;
     int columnsCount = worksheet.Dimension.Columns;
 
     var topics = new List<Topic>();
 
-    // Read hierarchical data from Excel cells
     for (int i = 2; i <= rowCount; i++)
     {
         for (int j = columnsCount; j > 1; j--)
@@ -26,15 +21,7 @@ using (ExcelPackage package = new ExcelPackage(new System.IO.FileInfo(filePath))
             {
                 topics.Add(new Topic() { Name = worksheet.Cells[i, j].Value?.ToString(), Row = i, Columns = j, Children = null, Show = true});
             }
-            //else
-            //{
-            //    topics.Add((new Topic() { Name = worksheet.Cells[i, j].Value?.ToString(), Row = i, Columns = j, Children = null, Show = true}));
-            //}
         }
-        // Assuming the hierarchical data is in the first column
-
-        // Output hierarchical data (you can process it further as needed)
-        //Console.WriteLine(hierarchicalData);
     }
 
     foreach (var topic in topics.OrderBy(c => c.Columns).ToList())
@@ -54,18 +41,18 @@ using (ExcelPackage package = new ExcelPackage(new System.IO.FileInfo(filePath))
         }
         else
         {
-            maxRows = rowCount;
+            maxRows = rowCount + 1;
         }
 
         var childrens = topics.Where(c => c.Columns == columns).Where(c => c.Row >= rows).Where(c => c.Row < maxRows)
             .ToList();
 
-
+        topics.Where(c => c.Columns == columns).Where(c => c.Row >= rows).Where(c => c.Row < maxRows).ToList().ForEach(c => c.Show = false);
         topic.Children = childrens;
     }
 
 
-    topics = topics.OrderBy(c => c.Columns).ToList();
+    topics = topics.OrderBy(c => c.Columns).Where(c => c.Show).ToList();
 }
 
 
